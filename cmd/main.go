1c691edb8ad441d5eff4c967a256b651b64f97d8
Matt Mannion <mattpmannion@gmail.com>
2022-10-24 10:32:59 -0400

    changed dev

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"_/cmd/src/repo"
 	"_/cmd/src/router"
 	"_/cmd/src/service"
+	"encoding/json"
+	"net/http"
 )
 
 var pr repo.PostRepo = repo.NewFirestoreRepo()
@@ -13,6 +15,12 @@ var pc controller.PostController = controller.NewPostController(ps)
 var r router.Router = router.NewMuxRouter()
 
 func main() {
+	r.Get("/hello", func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Content-type", "application-json")
+
+		resp.WriteHeader(http.StatusOK)
+		json.NewEncoder(resp).Encode(map[string]string{"message": "hello"})
+	})
 	r.Get("/", pc.GetPosts)
 	r.Post("/", pc.PostPost)
 
