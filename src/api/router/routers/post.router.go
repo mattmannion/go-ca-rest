cdb6bd743c62703d53be9ca8600d1d8c74e784df
Matt Mannion <mattpmannion@gmail.com>
2022-12-16 04:34:33 -0500

    interface improvements

--- src/api/router/routers/post.router.go
+++ src/api/router/routers/post.router.go
@@ -9,14 +9,14 @@ import (
 const url string = constants.ApiPrefixV1 + "/posts"
 
 type PostRouter struct {
-	router router_types.TMux
+	mux router_types.TMux
 }
 
 func NewPostRouter(router router_types.IMux, controller controller.ControllerLayer) *PostRouter {
-	return &PostRouter{router: router_types.TMux{Mux: router, Ctrlr: controller}}
+	return &PostRouter{mux: router_types.TMux{Mux: router, Ctrlr: controller}}
 }
 
 func (pr *PostRouter) Register() {
-	pr.router.Mux.Get(url, pr.router.Ctrlr.PostController.GetPosts)
-	pr.router.Mux.Post(url, pr.router.Ctrlr.PostController.PostPost)
+	pr.mux.Mux.Get(url, pr.mux.Ctrlr.PostController.GetPosts)
+	pr.mux.Mux.Post(url, pr.mux.Ctrlr.PostController.PostPost)
 }
